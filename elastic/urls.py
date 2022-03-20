a2907bc00efa48c9f5d3c70cf1537291f0aac2b2
maxcore25 <mkorobov2002@mail.ru>
2022-03-20 12:35:10 +0300

    Migrations Made

--- elastic/urls.py
+++ elastic/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import *
+
 urlpatterns = [
-    path('' , index),
-    path('search/' , PublisherDocumentView.as_view({'get': 'list'})),
+    path('', index),
+    path('search/', PublisherDocumentView.as_view({'get': 'list'})),
     path('admin/', admin.site.urls),
 ]
